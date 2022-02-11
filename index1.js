d0189dd788a5e59c25b8ba6fcc3e2b62f412c265
parth-17 <81242046+parth-17@users.noreply.github.com>
2022-02-11 13:14:41 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const sessionController = require("./controller/session-controller")
+
+
+const app = express()
+
+app.get("/",function(req,res){
+    res.write("welcome...")
+    res.end()
+})
+
+          
+app.get("/login",sessionController.login)
+app.get("/signup",sessionController.signup) 
+
+app.listen(3000,function(){
+  console.log("server started on 3000");  
+})
