a45ca767c7e38eeb08955ef4a00bead5eb1f51c8
parth <parthpatelunos2000@gmail.com>
2022-02-15 14:32:09 +0530

    database connected
    mongodb database was connected

--- index1.js
+++ index1.js
@@ -1,11 +1,25 @@
 const express = require("express")
-const sessionController = require("./controller/session-controller")
+const mongoose = require("mongoose")
 
+const sessionController = require("./controller/session-controller")
+const roleController = require("./controller/role-controller")
 
 const app = express()
 //middle ware
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+//database
+mongoose.connect('mongodb://localhost:27017/OFS',function(err){
+  if(err){
+    console.log("db connection fail..");
+    console.log(err);
+  }else{
+    console.log("db connected..");
+  }
+})
+
+//urls
 
 app.get("/",function(req,res){
     res.write("welcome...")
@@ -17,6 +31,11 @@ app.get("/login",sessionController.login)
 app.get("/signup",sessionController.signup) 
 app.get("/saveUser",sessionController.saveUser)
 
+//role
+app.post("/roles",roleController.addRole)
+
+
+//server
 app.listen(3000,function(){
   console.log("server started on 3000");  
 })
