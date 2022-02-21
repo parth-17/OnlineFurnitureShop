00f9a4f7f1bdc78c0ef70f45e5aab591c2da8812
parth <parthpatelunos2000@gmail.com>
2022-02-21 14:59:41 +0530

    category and subcategory added
    category and subcategory added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const sessionController = require("./controller/session-controller")
 const roleController = require("./controller/role-controller")
 const userController = require("./controller/user-controller")
+const categoryController = require("./controller/category-controller")
+const SubcategoryController = require("./controller/subcategory-controller")
 
 const app = express()
 //middle ware
@@ -28,21 +30,34 @@ app.get("/",function(req,res){
 })
 
           
-app.get("/login",sessionController.login)
-app.get("/signup",sessionController.signup) 
-app.post("/saveUser",sessionController.saveUser)
+app.get("/login", sessionController.login)
+app.get("/signup", sessionController.signup) 
+app.post("/saveUser", sessionController.saveUser)
 
 //role
-app.post("/roles",roleController.addRole)
-app.get("/roles",roleController.getAllRoles)
-app.delete("/roles/:roleId",roleController.deleteRole)
-app.put("/roles",roleController.updateRole)
+app.post("/roles", roleController.addRole)
+app.get("/roles", roleController.getAllRoles)
+app.delete("/roles/:roleId", roleController.deleteRole)
+app.put("/roles", roleController.updateRole)
 
 //user
-app.post("/users",userController.addUser)
-app.get("/users",userController.getAllUsers)
-app.delete("/users/:userId",userController.deleteUser)
+app.post("/users", userController.addUser)
+app.get("/users", userController.getAllUsers)
+app.delete("/users/:userId", userController.deleteUser)
+app.put("/users", userController.updateUser)
+app.post("/login",userController.login)
 
+//category
+app.post("/categories", categoryController.addCategory)
+app.get("/categories", categoryController.getAllCategories)
+app.delete("/categories/:categoryId", categoryController.deleteCategory)
+app.put("/categories", categoryController.updateCategory)
+ 
+//subcategory
+app.post("/subcategories", SubcategoryController.addSubcategories)
+app.get("/subcategories", SubcategoryController.getAllSubcategories)
+app.delete("/subcategories/:subcategoryId", SubcategoryController.deleteSubcategories)
+app.put("/subcategories", SubcategoryController.updateSubcategories)
 
 //server
 app.listen(3000,function(){
